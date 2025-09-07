4e847b8a7d3e8b897e729df95b4f1eb54dacef39
SebasLee98 <sebastianhclee@gmail.com>
2025-09-06 21:06:58 -0700

    Answer

--- ToyStoreData.sql
+++ ToyStoreData.sql
@@ -1,3 +1,10 @@
--- Identify customers on file that did not order a a vintage car
+-- Identify customers on file that did not order a vintage car
 
-SELECT * FROM customers
+With First_Query AS (
+SELECT A.customerName, B.orderNumber, D.productLine FROM customers A
+JOIN orders B ON A.customerNumber = B.customerNumber
+JOIN orderdetails C ON B.orderNumber = C.orderNumber
+JOIN products D ON C.productCode = D.productCode  
+)
+SELECT * FROM First_Query WHERE productLine <> 'Vintage Cars'
+ORDER BY productLine DESC
