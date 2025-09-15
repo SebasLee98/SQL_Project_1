be18469f90087492bfb4a349948ad729f41af825
SebasLee98 <sebastianhclee@gmail.com>
2025-09-15 13:35:15 -0700

    additions

--- ToyStoreData.sql
+++ ToyStoreData.sql
@@ -1,13 +1,14 @@
 -- Identify customers on file that did not order a vintage car
-
 With First_Query AS (
-SELECT A.customerName, D.productLine, C.quantityOrdered FROM customers A
+SELECT A.customerName, C.orderNumber, D.productLine, C.productCode, C.quantityOrdered FROM customers A
 JOIN orders B ON A.customerNumber = B.customerNumber
 JOIN orderdetails C ON B.orderNumber = C.orderNumber
 JOIN products D ON C.productCode = D.productCode  
 )
-SELECT distinct *
-FROM First_Query WHERE productLine <> 'Vintage Cars' ORDER BY customerName ASC;
+SELECT distinct customerName, count(DISTINCT orderNumber) AS Number_of_orders_made
+FROM First_Query GROUP BY customerName ORDER BY Number_of_orders_made DESC;
 
--- How many variations of the product category
-SELECT count(distinct productCode), productLine FROM products GROUP BY productLine
+-- Most popular products ranked
+SELECT B.productName, A.quantityOrdered FROM orderdetails A
+LEFT JOIN products B ON A.productCode = B.productCode
+ORDER BY quantityOrdered DESC LIMIT 15
